69ed433e3751824bbdbb8589f2b590660a4f11e5
dandohz <ramadhani.noor.pratama@gmail.com>
2023-02-26 20:49:01 +0800

    tampilan publik

---
+++ application/views/header.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+   <!-- Basic -->
+   <meta charset="utf-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <!-- Mobile Metas -->
+   <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+   <!-- Site Metas -->
+   <title>Game Info</title>
+   <meta name="keywords" content="">
+   <meta name="description" content="">
+   <meta name="author" content="">
+   <!-- Site Icons -->
+   <link rel="shortcut icon" href="" type="sports-master/image/x-icon" />
+   <link rel="apple-touch-icon" href="">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="sports-master/css/bootstrap.min.css">
+   <!-- Site CSS -->
+   <link rel="stylesheet" href="sports-master/style.css">
+   <!-- Colors CSS -->
+   <link rel="stylesheet" href="sports-master/css/colors.css">
+   <!-- ALL VERSION CSS -->	
+   <link rel="stylesheet" href="sports-master/css/versions.css">
+   <!-- Responsive CSS -->
+   <link rel="stylesheet" href="sports-master/css/responsive.css">
+   <!-- Custom CSS -->
+   <link rel="stylesheet" href="sports-master/css/custom.css">
+   <!-- font family -->
+   <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+   <!-- end font family -->
+   <link rel="stylesheet" href="sports-master/css/3dslider.css" />
+   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+   <link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+   <script src="sports-master/js/3dslider.js"></script>
+   </head>
+   <body class="game_info" data-spy="scroll" data-target=".header">
+      <!-- LOADER -->
+      <div id="preloader">
+         <img class="preloader" src="sports-master/images/loading-img.gif" alt="">
+      </div>
+      <!-- END LOADER -->
+      <section id="top">
+         <header>
+            <div class="container">
+               <div class="header-top">
+                  <div class="row">
+                     <div class="col-md-6">
+                        <div class="full">
+                           <div class="logo">
+                              <a href="index.html"><img src="sports-master/images/logo.png" alt="#" /></a>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-md-6">
+                        <div class="right_top_section">
+                           <!-- social icon -->
+                           <ul class="social-icons pull-left">
+                              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                              <li><a class="youtube" href="#"><i class="fa fa-youtube-play"></i></a></li>
+                              <li><a class="pinterest" href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                           </ul>
+                           <!-- end social icon -->
+                           <!-- button section -->
+                           <ul class="login">
+                              <li class="login-modal">
+                                 <a href="#" class="login"><i class="fa fa-user"></i>Login</a>
+                              </li>
+                           </ul>
+                           <!-- end button section -->
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="header-bottom">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <div class="full">
+                           <div class="main-menu-section">
+                              <div class="menu">
+                                 <nav class="navbar navbar-inverse">
+                                    <div class="navbar-header">
+                                       <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
+                                       <span class="sr-only">Toggle navigation</span>
+                                       <span class="icon-bar"></span>
+                                       <span class="icon-bar"></span>
+                                       <span class="icon-bar"></span>
+                                       </button>
+                                       <a class="navbar-brand" href="#">Menu</a>
+                                    </div>
+                                    <div class="collapse navbar-collapse js-navbar-collapse">
+                                       <ul class="nav navbar-nav">
+                                          <li class="active"><a href="index.html">Home</a></li>
+                                          <li><a href="about.html">About</a></li>
+                                          <li><a href="team.html">Team</a></li>
+                                          <li><a href="news.html">News</a></li>
+                                          <li class="dropdown mega-dropdown">
+                                             <a href="match" class="dropdown-toggle" data-toggle="dropdown">Match<span class="caret"></span></a>				
+                                             <ul class="dropdown-menu mega-dropdown-menu">
+                                                <li class="col-sm-8">
+                                                   <ul>
+                                                      <li class="dropdown-header">Men Collection</li>
+                                                      <div id="menCollection" class="carousel slide" data-ride="carousel">
+                                                         <div class="carousel-inner">
+                                                            <div class="item active">
+                                                               <div class="banner-for-match"><a href="#"><img class="img-responsive" src="sports-master/images/match-banner1.jpg" alt="#" /></a></div>
+                                                            </div>
+                                                            <!-- End Item -->
+                                                            <div class="item">
+                                                               <div class="banner-for-match"><a href="#"><img class="img-responsive" src="sports-master/images/match-banner1.jpg" alt="#" /></a></div>
+                                                            </div>
+                                                            <!-- End Item -->
+                                                            <div class="item">
+                                                               <div class="banner-for-match"><a href="#"><img class="img-responsive" src="sports-master/images/match-banner1.jpg" alt="#" /></a></div>
+                                                            </div>
+                                                            <!-- End Item -->                                
+                                                         </div>
+                                                         <!-- End Carousel Inner -->
+                                                         <!-- Controls -->
+                                                         <a class="left carousel-control" href="#menCollection" role="button" data-slide="prev">
+                                                         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                                         <span class="sr-only">Previous</span>
+                                                         </a>
+                                                         <a class="right carousel-control" href="#menCollection" role="button" data-slide="next">
+                                                         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                                         <span class="sr-only">Next</span>
+                                                         </a>
+                                                      </div>
+                                                      <!-- /.carousel -->
+                                                   </ul>
+                                                </li>
+                                                <li class="col-sm-4">
+                                                   <ul class="menu-inner">
+                                                      <li class="dropdown-header">Next Matchs</li>
+                                                      <li><a href="#">Contrary vs classical</a></li>
+                                                      <li><a href="#">Discovered vs undoubtable</a></li>
+                                                      <li><a href="#">Contrary vs classical</a></li>
+                                                      <li><a href="#">Discovered vs undoubtable</a></li>
+                                                      <li><a href="#">Contrary vs classical</a></li>
+                                                      <li><a href="#">Discovered vs undoubtable</a></li>
+                                                      <li><a href="#">Contrary vs classical</a></li>
+                                                      <li><a href="#">Discovered vs undoubtable</a></li>
+                                                   </ul>
+                                                </li>
+                                             </ul>
+                                          </li>
+                                          <li><a href="blog.html">Blog</a></li>
+                                          <li><a href="contact.html">contact</a></li>
+                                       </ul>
+                                    </div>
+                                    <!-- /.nav-collapse -->
+                                 </nav>
+                                 <div class="search-bar">
+                                    <div id="imaginary_container">
+                                       <div class="input-group stylish-input-group">
+                                          <input type="text" class="form-control"  placeholder="Search" >
+                                          <span class="input-group-addon">
+                                          <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>  
+                                          </span>
+                                       </div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </header>
+         <div class="full-slider">
+            <div id="carousel-example-generic" class="carousel slide">
+               <!-- Indicators -->
+               <ol class="carousel-indicators">
+                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+               </ol>
+               <!-- Wrapper for slides -->
+               <div class="carousel-inner" role="listbox">
+                  <!-- First slide -->
+                  <div class="item active deepskyblue" data-ride="carousel" data-interval="5000">
+                     <div class="carousel-caption">
+                        <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12"></div>
+                        <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                           <div class="slider-contant" data-animation="animated fadeInRight">
+                              <h3>If you Don’t Practice<br>You <span class="color-yellow">Don’t Derserve</span><br>to win!</h3>
+                              <p>If you use this site regularly and would like to help keep the site on the Internet,<br>
+                                 please consider donating a small sum to help pay..
+                              </p>
+                              <button class="btn btn-primary btn-lg">Read More</button>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- /.item -->
+                  <!-- Second slide -->
+                  <div class="item skyblue" data-ride="carousel" data-interval="5000">
+                     <div class="carousel-caption">
+                        <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12"></div>
+                        <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                           <div class="slider-contant" data-animation="animated fadeInRight">
+                              <h3>If you Don’t Practice<br>You <span class="color-yellow">Don’t Derserve</span><br>to win!</h3>
+                              <p>You can make a case for several potential winners of<br>the expanded European Championships.</p>
+                              <button class="btn btn-primary btn-lg">Button</button>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- /.item -->
+                  <!-- Third slide -->
+                  <div class="item darkerskyblue" data-ride="carousel" data-interval="5000">
+                     <div class="carousel-caption">
+                        <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12"></div>
+                        <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                           <div class="slider-contant" data-animation="animated fadeInRight">
+                              <h3>If you Don’t Practice<br>You <span class="color-yellow">Don’t Derserve</span><br>to win!</h3>
+                              <p>You can make a case for several potential winners of<br>the expanded European Championships.</p>
+                              <button class="btn btn-primary btn-lg">Button</button>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- /.item -->
+               </div>
+               <!-- /.carousel-inner -->
+               <!-- Controls -->
+               <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+               <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+               <span class="sr-only">Previous</span>
+               </a>
+               <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+               <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+               <span class="sr-only">Next</span>
+               </a>
+            </div>
+            <!-- /.carousel -->
+            <div class="news">
+               <div class="container">
+                  <div class="heading-slider">
+                     <p class="headline"><i class="fa fa-star" aria-hidden="true"></i> Top Headlines :</p>
+                     <!--made by vipul mirajkar thevipulm.appspot.com-->
+                     <h1>
+                     <a href="" class="typewrite" data-period="2000" data-type='[ "Contrary to popular belief, Lorem Ipsum is not simply random text.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."]'>
+                     <span class="wrap"></span>
+                     </a>
+                     </h1	   
+                     <span class="wrap"></span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
