ee2c5d8e919a6b71b57c22c43cbc2532e93e6d77
selalualways <terong461@gmail.com>
2023-06-08 23:53:02 +0800

    s

--- application/views/publik/konten_depan.php
+++ application/views/publik/konten_depan.php
@@ -1,116 +1,10 @@
 <div class="matchs-info">
-         <div class="col-md-6 col-sm-6 col-xs-12">
-            <div class="row">
-               <div class="full">
-                  <div class="matchs-vs">
-                     <div class="vs-team">
-                        <div class="team-btw-match">
-                           <ul>
-                              <li>
-                                 <img src="sports-master/images/img-03.png" alt="">
-                                 <span>Footbal Team</span>
-                              </li>
-                              <li class="vs"><span>vs</span></li>
-                              <li>
-                                 <img src="sports-master/images/img-04.png" alt="">
-                                 <span>Super Team Club</span>
-                              </li>
-                           </ul>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <div class="col-md-6 col-sm-6 col-xs-12">
-            <div class="row">
-               <div class="full">
-                  <div class="right-match-time">
-                     <h2>Next Match</h2>
-                     <ul id="countdown-1" class="countdown">
-                        <li><span class="days">10 </span>Day </li>
-                        <li><span class="hours">5 </span>Hours </li>
-                        <li><span class="minutes">25 </span>Minutes </li>
-                        <li><span class="seconds">10 </span>Seconds </li>
-                     </ul>
-                     <span>12/02/2017 /19:00pm</span>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
+         
+      
       <section id="contant" class="contant">
          <div class="container">
             <div class="row">
-               <div class="col-lg-4 col-sm-4 col-xs-12">
-                  <aside id="sidebar" class="left-bar">
-                     <div class="banner-sidebar">
-                        <img class="img-responsive" src="sports-master/images/img-05.jpg" alt="#" />
-                        <h3>Lorem Ipsum is simply dummy text..</h3>
-                     </div>
-                  </aside>
-                  <h4>Match Fixture</h4>
-                  <aside id="sidebar" class="left-bar">
-                     <div class="feature-matchs">
-                        <div class="team-btw-match">
-                           <ul>
-                              <li>
-                                 <img src="sports-master/images/img-01_002.png" alt="">
-                                 <span>Portugal</span>
-                              </li>
-                              <li class="vs"><span>vs</span></li>
-                              <li>
-                                 <img src="sports-master/images/img-02.png" alt="">
-                                 <span>Germany</span>
-                              </li>
-                           </ul>
-                           <ul>
-                              <li>
-                                 <img src="sports-master/images/img-03_002.png" alt="">
-                                 <span>Portugal</span>
-                              </li>
-                              <li class="vs"><span>vs</span></li>
-                              <li>
-                                 <img src="sports-master/images/img-04_003.png" alt="">
-                                 <span>Germany</span>
-                              </li>
-                           </ul>
-                           <ul>
-                              <li>
-                                 <img src="sports-master/images/img-05_002.png" alt="">
-                                 <span>Portugal</span>
-                              </li>
-                              <li class="vs"><span>vs</span></li>
-                              <li>
-                                 <img src="sports-master/images/img-06.png" alt="">
-                                 <span>Germany</span>
-                              </li>
-                           </ul>
-                           <ul>
-                              <li>
-                                 <img src="sports-master/images/img-07_002.png" alt="">
-                                 <span>Portugal</span>
-                              </li>
-                              <li class="vs"><span>vs</span></li>
-                              <li>
-                                 <img src="sports-master/images/img-08.png" alt="">
-                                 <span>Germany</span>
-                              </li>
-                           </ul>
-                           <ul>
-                              <li>
-                                 <img src="sports-master/images/img-05_002.png" alt="">
-                                 <span>Portugal</span>
-                              </li>
-                              <li class="vs"><span>vs</span></li>
-                              <li>
-                                 <img src="sports-master/images/img-06.png" alt="">
-                                 <span>Germany</span>
-                              </li>
-                           </ul>
-                        </div>
-                     </div>
-                  </aside>
+               <div class="col-lg-4 col-sm-4 col-xs-12">              
                   <h4>Points Table</h4>
                   <aside id="sidebar" class="left-bar">
                      <div class="feature-matchs">
@@ -171,7 +65,7 @@
                         </table>
                      </div>
                   </aside>
-                  <div class="content-widget top-story" style="background: url(sports-master/images/top-story-bg.jpg);">
+                  <div class="content-widget top-story" style="background: url(sports-master/images/bg-perbasi.jpg);">
                      <div class="top-stroy-header">
                         <h2>Top Soccer Headlines Story <a href="#" class="fa fa-fa fa-angle-right"></a></h2>
                         <span class="date">July 05, 2017</span>
@@ -222,96 +116,4 @@
             </div>
          </div>
       </section>
-      <section>
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12">
-                  <div class="full">
-                     <div class="main-heading sytle-2">
-                        <h2>Video</h2>
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium<br>doloremque laudantium, totam rem aperiam</p>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </section>
-      <section class="video_section_main theme-padding middle-bg vedio">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12">
-                  <div class="full">
-                     <div class="match_vedio">
-                        <img class="img-responsive" src="sports-master/images/img-07.jpg" alt="#" />
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </section>
-      <div class="team-holder theme-padding">
-         <div class="container">
-            <div class="main-heading-holder">
-               <div class="main-heading sytle-2">
-                  <h2>Meet Your Team</h2>
-                  <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium<br>doloremque laudantium, totam rem aperiam</p>
-               </div>
-            </div>
-            <div id="team-slider">
-               <div class="container">
-                  <div class="col-md-3">
-                     <div class="team-column style-2">
-                        <img src="sports-master/images/img-1-1.jpg" alt="">
-                        <div class="player-name">
-                           <div class="desination-2">Defender</div>
-                           <h5>Charles Wheeler</h5>
-                           <span class="player-number">12</span>
-                        </div>
-                        <div class="overlay">
-                           <div class="team-detail-hover position-center-x">
-                              <p>Lacus vulputate torquent mollis venenatis quisque suspendisse fermentum primis,</p>
-                              <ul class="social-icons style-4 style-5">
-                                 <li><a class="facebook" href="#" tabindex="0"><i class="fa fa-facebook"></i></a></li>
-                                 <li><a class="twitter" href="#" tabindex="0"><i class="fa fa-twitter"></i></a></li>
-                                 <li><a class="youtube" href="#" tabindex="0"><i class="fa fa-youtube-play"></i></a></li>
-                                 <li><a class="pinterest" href="#" tabindex="0"><i class="fa fa-pinterest-p"></i></a></li>
-                              </ul>
-                              <a class="btn blue-btn" href=" /soccer/team-detail.html" tabindex="0">View Detail</a>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-md-3">
-                     <div class="team-column style-2">
-                        <img src="sports-master/images/img-1-2.jpg" alt="">
-                        <div class="player-name">
-                           <div class="desination-2">Defender</div>
-                           <h5>Charles Wheeler</h5>
-                           <span class="player-number">12</span>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-md-3">
-                     <div class="team-column style-2">
-                        <img src="sports-master/images/img-1-3.jpg" alt="">
-                        <div class="player-name">
-                           <div class="desination-2">Defender</div>
-                           <h5>Charles Wheeler</h5>
-                           <span class="player-number">12</span>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-md-3">
-                     <div class="team-column style-2">
-                        <img src="sports-master/images/img-1-4.jpg" alt="">
-                        <div class="player-name">
-                           <div class="desination-2">Defender</div>
-                           <h5>Charles Wheeler</h5>
-                           <span class="player-number">12</span>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
+      
