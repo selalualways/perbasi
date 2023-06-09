14eddd7bb1539e8e0d25dfe60da566b8b5d672dd
selalualways <terong461@gmail.com>
2023-06-09 14:47:11 +0800

    ubah imagesantai

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -59,11 +59,11 @@
     <!-- Search Bar -->
     <div class="search-bar">
         <div class="search-icon">
-            <i class="material-icons">search</i>
+            <i class="material-icons">Search</i>
         </div>
         <input type="text" placeholder="START TYPING...">
         <div class="close-search">
-            <i class="material-icons">close</i>
+            <i class="material-icons">Close</i>
         </div>
     </div>
     <!-- #END# Search Bar -->
